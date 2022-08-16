2fb5c1810570a9b4da6b8403af1fbe9e90084ab6
Nick Matthews <nmatthews@perforce.com>
2022-08-16 12:38:00 +0100

    sources added

---
+++ test3.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+
+static int32_t count;                 /* "count" has internal linkage             */
+static void foo ( void )              /* "foo" has internal linkage               */
+{
+  int16_t count;                      /* Non-compliant - "count" has no linkage
+                                       * but clashes with an identifier with
+                                       * internal linkage                         */
+  int16_t index;                      /* "index" has no linkage                   */
+}
+
+void bar1 ( void ) 
+{
+  static int16_t count;               /* Non-compliant - "count" has no linkage
+                                       * but clashes with an identifier with
+                                       * internal linkage                         */
+  int16_t index;                      /* Compliant - "index" is not unique but
+                                       * has no linkage                           */
+  foo ( ); 
+}
---
+++ test6.c
@@ -0,0 +1,28 @@
+
+#include <stdint.h>
+/* file2.c */ 
+
+
+
+static int8_t count;                  /* Non-compliant - "count" has internal
+                                       * linkage but clashes with other
+                                       * identifiers of the same name             */
+static void foo ( void )              /* Non-compliant - "foo" has internal
+                                       * linkage but clashes with a function of
+                                       * the same name                            */
+{
+  int32_t index;                      /* Compliant - both "index" and "nbytes"    */
+  int16_t nbytes;                     /* are not unique but have no linkage       */
+}
+void bar2 ( void ) 
+{
+  static uint8_t nbytes;              /* Compliant - "nbytes" is not unique but
+                                       * has no linkage and the storage class is
+                                       * irrelevant                               */
+}
+
+void badasbing( void )
+{
+	static uint32_t data;
+};
+
